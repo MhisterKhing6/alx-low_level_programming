584569488b1ce532c5893d57950a6c55d910946e
Kingsley <dondecency11@gmail.com>
2022-06-30 15:36:21 +0000

    Last Mandatory

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - Print to the std errr
+ * Return: 1
+ */
+int main(void)
+{
+fprint(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n")
+return (10);
+}
