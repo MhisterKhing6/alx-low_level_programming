72b4006e6c9fae4cfcfdf290b995e61ca0ab20fe
Kingsley <dondecency11@gmail.com>
2022-06-30 14:27:56 +0000

    Third Page

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
 
+/**
+ * main - prints "Promgramming is like a building a multilingual puzzle"
+ * Return: 0
+ */
 int main(void)
 {
-  puts("\"Programming is like building a multilingual puzzle\n");
+puts("\"Programming is like building a multilingual puzzle\n");
 
-  return (0);
+return (0);
 
 }
