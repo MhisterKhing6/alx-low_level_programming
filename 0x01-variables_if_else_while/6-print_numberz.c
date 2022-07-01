481f6280514017233c97f18b7903c98d5a7f814d
Kingsley <dondecency11@gmail.com>
2022-07-01 08:15:50 +0000

    6 commit

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main- print the alphabets in lower case
+ * Return: 0
+ */
+
+int main(void)
+{
+int start = 0;
+while (start <= 9)
+putchar(start++ + '0');
+putchar('\n');
+return (0);
+}
