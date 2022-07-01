84b1a12ccbc6eed2c8d5dcd52a1b68a9818b8b88
Kingsley <dondecency11@gmail.com>
2022-07-01 08:00:09 +0000

    Sith Task

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main- print the alphabets in lower case
+ * Return: 0
+ */
+
+int main(void)
+{
+int start = 0;
+while (start <= 9)
+printf("%d", start++);
+putchar('\n');
+return (0);
+}
