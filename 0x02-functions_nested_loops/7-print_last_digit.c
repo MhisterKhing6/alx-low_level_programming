b7e93901c313f939ac351dbba4bc9f2cb2bcd002
Kingsley <dondecency11@gmail.com>
2022-07-05 13:28:02 +0000

    7 th

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_last_digit;- Print the absolute value of the number'
+ * Return: 0
+ * @c : integer to check abs value
+*/
+int print_last_digit(int c);
+{
+int l = c % 10;
+_putchar(c + '0');
+return (l);
+}
