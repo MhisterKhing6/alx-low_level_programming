f17d362d139e9ec317051302cf955cdd0898136e
Kingsley <dondecency11@gmail.com>
2022-07-05 09:48:57 +0000

    Fist Push

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,18 +1,16 @@
 #include <stdio.h>
 #include "main.h"
+
 /**
  * print_alphabet_x10- print alphabet in lower case'
  * return 0
  */
 void print_alphabet_x10(void)
 {
-for (int i = 0; i < 10; i++)
-{
 char start = 'a';
 while (start <= 'z')
 _putchar(start++);
-_putchar('\n');
-}
+putchar('\n');
 }
 
 /**
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+This is concept about functions in c 
