dd82ac4b41a77570608443df346a3473e08575de
Kingsley <dondecency11@gmail.com>
2022-07-05 12:47:45 +0000

    how

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -12,22 +12,7 @@ return (1);
 else
 return (0);
 }
-/**
- * main - print _puchar on the command line
- * Return: 0
-*/
-int main(void)
-{
-int r;
-r = _islower('H');
-_putchar(r + '0');
-r = _islower('o');
-_putchar(r + '0');
-r = _islower(108);
-_putchar(r + '0');
-_putchar('\n');
-return (0);
-}
+
 
 
 
