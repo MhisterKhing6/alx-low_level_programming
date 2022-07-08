faf55e89e5410a3fd0281bbb3f4c4ae80125f577
Kingsley <dondecency11@gmail.com>
2022-07-08 09:08:28 +0000

    How

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _isupper - check if a string is Lower and returns it'
+ * @c : the character to check
+ * Return: 1 is c is lower else 0
+*/
+int _isupper(int c)
+{
+if (c >= 'A' && c <= 'Z')
+return (1);
+else
+return (0);
+}
+
+
+
+
+
