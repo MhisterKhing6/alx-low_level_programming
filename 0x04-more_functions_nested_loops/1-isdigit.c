7f3d4249a73364117d4d875f822b19d24ce921f8
Kingsley <dondecency11@gmail.com>
2022-07-08 09:41:59 +0000

    how

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
 #include "main.h"
+#include <ctype.h>
 /**
- * _is - check if a string is lower and returns it'
+ * _isdigit - check if a string is lower and returns it'
  * @c : the character to check
  * Return: 1 is c is lower else 0
 */
 int _isdigit(int c)
 {
-if (c < 0 || c > 9)
-return (0);
-else
-return (1);
+return (isdigit(c));
 }
+
+
 
 
 
