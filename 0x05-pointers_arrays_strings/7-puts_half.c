7591603430fa6cf305934940ee00ad41d35d6e08
Kingsley <dondecency11@gmail.com>
2022-07-11 11:03:41 +0000

    how

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+#include "string.h"
+/**
+ * puts_half - print String in reverse order
+ * @n: a parameter to reset
+ */
+void puts_half(char *n)
+{
+unsigned long i = strlen(n) % 2 == 0 ? strlen(n) / 2 : (strlen(n) - 1) / 2;
+while (i < strlen(n))
+putchar(n[i++]);
+putchar('\n');
+}
