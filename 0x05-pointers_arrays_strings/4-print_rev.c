1e5716b4260381e64e55ee8abe94b6cdecf2908c
Kingsley <dondecency11@gmail.com>
2022-07-11 09:59:25 +0000

    how

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,19 +1,14 @@
 #include <stdio.h>
 #include "main.h"
+#include "string.h"
 /**
  * print_rev - print String in reverse order
  * @n: a parameter to reset
  */
 void print_rev(char *n)
 {
-char stop = '\0';
-int i = 0;
-while (*(n + i++) != stop)
-{};
-i = i - 1;
-
-while (i > 0)
+int i = strlen(n) - 1;
+while (i >= 0)
 putchar(n[i--]);
 putchar('\n');
 }
-
