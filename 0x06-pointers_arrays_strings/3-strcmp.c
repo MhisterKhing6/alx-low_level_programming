c34d41c46cd577a7c2bc94964c01b79c5fa4b683
Kingsley <dondecency11@gmail.com>
2022-07-13 10:01:47 +0000

    3 commit

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+#include <string.h>
+#include <stdlib.h>
+/**
+ * _strcmp - print strings to the console
+ * @dest: a holder to contained the copied file
+ * @src: a holder to copy string from
+ * Return: an integer representing the if equal or not
+ */
+int  _strcmp(char *dest, char *src)
+{
+return (strcmp(dest, src));
+}
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,6 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 char * _strncat(char * , char * , int);
+int _strcmp(char * , char *);
 int _atoi(char *);
 #include <stdio.h>
 char * _strncpy(char *, char *, int);
