8ebc48a2b785b0a5fb2588a7089867bb1fc64c31
Kingsley <dondecency11@gmail.com>
2022-07-13 10:56:19 +0000

    how

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+#include <stdlib.h>
+#include <string.h>
+/**
+ * print_number - print strings to the console
+ * @i: a holder to copy string from
+ * Return: returns a pointer to the copied folder
+ */
+void print_number(int i)
+{
+printf("%d", i);
+}
