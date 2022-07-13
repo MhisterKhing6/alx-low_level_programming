90dac04deccd57357ccea1e4ab7169a24d472931
Kingsley <dondecency11@gmail.com>
2022-07-13 09:39:03 +0000

    First commit

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+#include <string.h>
+/**
+ * _strcat - print strings to the console
+ * @dest: a holder to contained the copied file
+ * @src: a holder to copy string from
+ * Return: returns a pointer to the copied folder
+ */
+char  *_strcpy(char *dest, char *src)
+{
+return (strcat(dest, src));
+}
---
+++ 0x06-pointers_arrays_strings/main.h
@@ -0,0 +1,17 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+int _atoi(char *);
+#include <stdio.h>
+void puts_half(char *);
+void put2(char *);
+void print_rev(char *);
+int _strlen(char *);
+void swap_int(int *a, int *b);
+void positive_or_negative(int i);
+void reset_to_98(int *n);
+int largest_number(int a, int b, int c);
+void print_remaining_days(int month, int day, int year);
+void print_array(int * , int );
+char * _strcat(char * , char *);
+#endif /* MAIN_H */
