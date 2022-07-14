0314297005bc66902bb6ef795320a0e7ffe31cf5
Kingsley <dondecency11@gmail.com>
2022-07-14 09:00:11 +0000

    how

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+#include <string.h>
+/**
+ * _strcpy - print strings to the console
+ * @dest: a holder to contained the copied file
+ * @src: a holder to copy string from
+ * Return: returns a pointer to the copied folder
+ */
+char  * string_toupper(char *dest)
+{
+return (strupr(dest));
+}
