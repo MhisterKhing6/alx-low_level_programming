cfd415de2471d71815c830b0f674e0de9aa41346
Kingsley <dondecency11@gmail.com>
2022-07-14 13:38:36 +0000

    how

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+#include <string.h>
+/**
+ * string_toupper - print strings to the console
+ * @dest: a holder to contained the copied file
+ * Return: returns a pointer to the copied folder
+ */
+char  *leet(char *dest)
+{
+int size = strlen(dest);
+int i = 0;
+for (; i < size; i++)
+{
+if (dest[i] == 'a' || dest[i]  == 'A')
+dest[i] = 4 + '0';
+
+if (dest[i] == 'e' || dest[i]  == 'E')
+dest[i] = 3 + '0';
+
+if (dest[i] == 'o' || dest[i]  == 'O')
+dest[i] = 0 + '0';
+
+if (dest[i] == 't' || dest[i]  == 'T')
+dest[i] = 7 + '0';
+
+if (dest[i] == 'l' || dest[i]  == 'L')
+dest[i] = 1 + '0';
+}
+return (dest);
+}
