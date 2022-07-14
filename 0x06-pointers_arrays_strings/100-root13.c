8cc260ff632520cf3c908f0e23adba3d38a882d0
Kingsley <dondecency11@gmail.com>
2022-07-14 14:15:40 +0000

    how

---
+++ 0x06-pointers_arrays_strings/100-root13.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+#include <string.h>
+/**
+ * root13 - print strings to the console
+ * @dest: a holder to contained the copied file
+ * Return: returns a pointer to the copied folder
+ */
+char *root13(char *dest)
+{
+int size = strlen(dest);
+int i = 0;
+int add = 0;
+for (; i < size; i++)
+{
+if ((dest[i] >= 'a' && dest[i] <= 'z') || (dest[i] >= 'A' && <= 'Z'))
+{
+dest[i] += 13;
+}
+}
+return (dest);
+}
