9450065d4e9b8743f676876c56962b9b6aa7c051
Kingsley <dondecency11@gmail.com>
2022-07-18 06:19:02 +0000

    ho

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,5 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
+char * _strstr(char * , char *);
 char * _strpbrk(char * , char *);
 unsigned int _strspn(char * , char *);
 char * _memcpy(char * , char * , unsigned int);
