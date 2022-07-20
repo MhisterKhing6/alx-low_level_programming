42d782dec7a3c1020dd68f49fa060c80b159be81
Kingsley <dondecency11@gmail.com>
2022-07-20 13:02:03 +0000

    How

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,7 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 int factorial(int);
-int pow_recursion(int, int);
+int _pow_recursion(int, int);
 int _strlen_recursion(char *);
 void _print_rev_recursion(char *);
 void print_chessboard(char (*a)[8]);
