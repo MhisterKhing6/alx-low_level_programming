78d60d070e39f74d648adeddc57e6ab967aa08d9
Kingsley <dondecency11@gmail.com>
2022-07-20 12:51:46 +0000

    4th push

---
+++ 0x08-recursion/4-pow.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+/**
+ * _pow_recursion- calculate the factorial of a number
+ * @b: a number to raise the power to
+ * @n: a number to calculate the factorial
+ * Return: the calculated factorial of n
+ */
+
+int _pow_recursion(int n, int b)
+{
+return (pow(n, b));
+}
