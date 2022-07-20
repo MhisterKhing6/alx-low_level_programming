a88087a93447cff8876b77ae6ef3e52e6c15baec
Kingsley <dondecency11@gmail.com>
2022-07-20 12:35:34 +0000

    how

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,6 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
-int _strlen_recursion.c
+int _strlen_recursion(char *);
 void _print_rev_recursion(char *);
 void print_chessboard(char (*a)[8]);
 void _puts_recursion(char *);
