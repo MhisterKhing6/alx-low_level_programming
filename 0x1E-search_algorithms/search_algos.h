5e9c6f910060c4cc9b3008498ce43c70ae5551cd
Kingsley <dondecency11@gmail.com>
2023-01-27 23:15:52 +0000

    w

---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,9 @@
+#ifndef SEARCH_ALGOS
+#include "stdio.h"
+#define SEARCH_ALGOS
+int linear_search(int *, size_t, int);
+void print_ar(int  *, size_t, size_t );
+int bin(int *, size_t, size_t, int);
+int binary_search(int *, size_t, int);
+
+#endif
