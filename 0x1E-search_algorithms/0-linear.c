f8d42c310285280edf5926184203a1c59218c8d6
Kingsley <dondecency11@gmail.com>
2023-01-27 23:24:10 +0000

    2

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -14,9 +14,10 @@ int linear_search(int *array, size_t size, int value)
 {
 if (array)
 {
-for (size_t i = 0; i < size; i++)
+size_t i = 0;
+for (; i < size; i++)
 {
-printf("Value checked array[%lu] = [%d]\n", i, array[i]);
+printf("Value checked array[%lu] = [%d]\n", i,array[i]);
 if (array[i] == value)
 return (i);
 }
